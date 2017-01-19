82fddc9aa3fcbdc9ffdcb5e7033870d7e2d82a2b
liuzenglu <liuzenglu@duoduosports.com>
2017-01-19 14:33:08 +0800

    fix

--- warpwallet_cracker.go
+++ warpwallet_cracker.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "golang.org/x/crypto/pbkdf2"
-	"golang.org/x/crypto/scrypt"
-	"bytes"
-	"crypto/sha256"
-	"fmt"
-	"time"
-	"os"
-	"math/rand"
-	"github.com/vsergeev/btckeygenie/btckey"
+  "golang.org/x/crypto/pbkdf2"
+  "golang.org/x/crypto/scrypt"
+  "bytes"
+  "crypto/sha256"
+  "fmt"
+  "time"
+  "os"
+  "math/rand"
+  "github.com/hk185/btckeygenie/btckey"
 )
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const (
@@ -39,37 +39,37 @@ func RandStringBytesMaskImprSrc(n int) string {
 }
 
 func main () {
-	var address string
-	saltValue := ""
-	
-	if len(os.Args) >= 2 {
-		address = os.Args[1]
-		if len(os.Args) == 3 {
-			saltValue = os.Args[2]
-		} else {
-			saltValue = "";
-		}
-	} else {
-		fmt.Printf("Usage: %s [Address] [Salt - optional]\n\n", os.Args[0])
-		os.Exit(0)
-	}
-	
-	fmt.Printf("Using address \"%s\" and salt \"%s\"\n", address, saltValue)
-	
-	tries := 0
-	start := time.Now()
-	for {
-		passphraseValue := RandStringBytesMaskImprSrc(8)
-		bruteforce(passphraseValue, saltValue, address);
-		tries += 1
-		fmt.Printf("\rTried %d passphrases in %s [last passphrase: %s]", tries, time.Since(start), passphraseValue)
-	}
+  var address string
+  saltValue := ""
+
+  if len(os.Args) >= 2 {
+    address = os.Args[1]
+    if len(os.Args) == 3 {
+      saltValue = os.Args[2]
+    } else {
+      saltValue = "";
+    }
+  } else {
+    fmt.Printf("Usage: %s [Address] [Salt - optional]\n\n", os.Args[0])
+    os.Exit(0)
+  }
+
+  fmt.Printf("Using address \"%s\" and salt \"%s\"\n", address, saltValue)
+
+  tries := 0
+  start := time.Now()
+  for {
+    passphraseValue := RandStringBytesMaskImprSrc(8)
+    bruteforce(passphraseValue, saltValue, address);
+    tries += 1
+    fmt.Printf("\rTried %d passphrases in %s [last passphrase: %s]", tries, time.Since(start), passphraseValue)
+  }
 }
 
 func bruteforce(passphraseValue string, saltValue string, address string) {
-	var priv btckey.PrivateKey
-	var err error
-	
+  var priv btckey.PrivateKey
+  var err error
+
     pass := fmt.Sprint(passphraseValue, "\x01")
     salt := fmt.Sprint(saltValue, "\x01")
     key, _ := scrypt.Key([]byte(pass), []byte(salt), 262144, 8, 1, 32)
@@ -82,19 +82,19 @@ func bruteforce(passphraseValue string, saltValue string, address string) {
         result.WriteByte(key[i] ^ key2[i])
     }
 
-	err = priv.FromBytes(result.Bytes())
-	if err != nil {
-		fmt.Printf("Error importing private key: %s [%s]\n", err, passphraseValue)
-		os.Exit(0)
-	}
-	
-	address_uncompressed := priv.ToAddressUncompressed()
-	wif := priv.ToWIF()
-	
-	if (address_uncompressed == address) {
-		fmt.Printf("Found! Passphrase      %s\n", passphraseValue)
-		fmt.Printf("Bitcoin Address (Uncompressed)      %s\n", address_uncompressed)
-		fmt.Printf("Private Key WIF (Uncompressed)      %s\n", wif)
-		os.Exit(0)
-	}
+  err = priv.FromBytes(result.Bytes())
+  if err != nil {
+    fmt.Printf("Error importing private key: %s [%s]\n", err, passphraseValue)
+    os.Exit(0)
+  }
+
+  address_uncompressed := priv.ToAddressUncompressed()
+  wif := priv.ToWIF()
+
+  if (address_uncompressed == address) {
+    fmt.Printf("Found! Passphrase      %s\n", passphraseValue)
+    fmt.Printf("Bitcoin Address (Uncompressed)      %s\n", address_uncompressed)
+    fmt.Printf("Private Key WIF (Uncompressed)      %s\n", wif)
+    os.Exit(0)
+  }
 }
